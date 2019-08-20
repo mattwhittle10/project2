6714324ea005fd0b477dbe44d6fbf39e69332268
Whittle <matt.whittle10@gmail.com>
2019-08-19 23:32:52 -0600

    folders and basic files created as well as added Schema & Seeds SQL for products table

--- seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO customers (first_name, last_name, email_address, phone_number, host_rating, first_time_login, google_id)
-VALUES ("Nathan","Elorreaga", "nathan.elorreaga@gmail.com", "18015584295", 5, FALSE, 114870151396533676186),
- ("Calli","Crase", "callilogancrase@gmail.com", "19718065927", 5, FALSE, 108542505935309325066),
- ("Michael","Skarn", "mikeSizzle@gmail.com", "19718065927", 5, TRUE, NULL ),
- ("Some","Body", "somebody@gmail.com", "19718065927", 5, TRUE, NULL),
- ("Marvin","Milano", "somebody@gmail.com", "19718065927", 5, TRUE, NULL);
