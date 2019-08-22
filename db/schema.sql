18dea77704158bcd91f5d4d37ee02587bd289a6f
Calli Crase <callilogancrase@gmail.com>
2019-08-21 22:45:03 -0600

    edited schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -8,25 +8,30 @@ CREATE TABLE customers (
   last_name  VARCHAR(45) NOT NULL,
   email_address VARCHAR(75) NOT NULL ,
   phone_number   VARCHAR(11),
-  host_rating INT NOT NULL,
-  first_time_login BOOLEAN NOT NULL DEFAULT TRUE,
+  host_rating INT NOT NULL DEFAULT 0,
+  first_time_login BOOLEAN NOT NULL DEFAULT true,
   google_id VARCHAR (255),
   PRIMARY KEY (customer_id)
 );
-
-DROP DATABASE IF EXISTS spaces_db;
-CREATE DATABASE spaces_db;
-USE spaces_db;
 
 CREATE TABLE products (
   product_id int AUTO_INCREMENT NOT NULL,
   title varchar(60) NOT NULL,
   img varbinary(max),
-  location varchar(30) NOT NULL,
-  available BOOLEAN,
+  address varchar(30) NOT NULL,
+  available BOOLEAN DEFAULT true,
   price INTEGER(1000) NOT NULL,
   category varchar (30) NOT NULL,
   description varchar(250) NOT NULL,
   FOREIGN KEY (product_id) REFERENCES customers(customer_id),
   PRIMARY KEY(product_id)
 );
+
+alternative idea for defining type
+indoor BOOLEAN DEFAULT false,
+secure BOOLEAN DEFAULT false, 
+total_acess BOOLEAN DEFAULT false, 
+
+
+
+
