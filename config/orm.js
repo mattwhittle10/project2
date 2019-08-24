f519e50a3358a669cb0d0451b08359740345bbb0
Whittle <matt.whittle10@gmail.com>
2019-08-24 12:14:32 -0600

    Saving changes before removing items from github

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,25 @@
 var connection = require('./connection');
 
 var orm = {
-    allListings: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-          cb(result);
-        });
-      },
-    listingsByZip: function(tableInput, colToSearch, valOfCol) {
-        var queryString = "SELECT * FROM ?? WHERE ?? = ??";
-        connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-          if (err) throw err;
-          console.log(result);
-        });
+  allListings: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
       }
+      cb(result);
+    });
+  },
+  listingsByZip: function (tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ??";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
 
 
-    // updateuser: function(table, username, firstname, lastname,)
+  // updateuser: function(table, username, firstname, lastname,)
 };
 
 module.exports = orm;
--- models/storage.js
+++ models/storage.js
@@ -1,13 +1,13 @@
 var orm = require('../config/orm');
 
-var listing ={
-    allListings: function(cb){
-        orm.allListings("products", function(res){
+var listing = {
+    allListings: function (cb) {
+        orm.allListings("products", function (res) {
             cb(res);
         });
     },
-    listingsByZip: function(cb){
-        orm.listingsByZip("products","zipcode",zipcodeInput, function(res){
+    listingsByZip: function (cb) {
+        orm.listingsByZip("products", "zipcode", zipcodeInput, function (res) {
             cb(res);
         })
     }
