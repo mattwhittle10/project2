ba4bb367d888a313b25787da589b42d9c13c8d79
Calli Crase <callilogancrase@gmail.com>
2019-08-24 11:16:47 -0600

    added orm data and storage.js model

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const listing = require('../models/storage');
 
   router.get("/", function (req, res) {
-    res.render("listings")
+    listing.allListings(function(data){
+      var listing = {
+        listings: data
+      }
+      console.log(listing);
+      res.render("listings", listing);
+    })
+
+    // listing.listingsByZip(function(data){
+    //   var hbsObject = {
+    //     listings: data
+    //   }
+    //   console.log(hbsObject);
+    //   res.render("listings", hbsObject);
+    // })
+    
   });
 
   module.exports = router;
