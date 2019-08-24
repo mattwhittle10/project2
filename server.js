d72ca6e93d998dbe17d3f25b6858055b9fd1b55b
Calli Crase <callilogancrase@gmail.com>
2019-08-24 10:27:44 -0600

    added listings route into htmlRoutes.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const profileRoutes = require('./routes/profile-routes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const passportSetup = require('./controller/passport-setup');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
@@ -36,6 +37,7 @@ app.set("view engine", "handlebars");
 //set up routes
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
+app.use('/listings', htmlRoutes);
 
 //create home route
 app.get('/', (req, res)=>{
