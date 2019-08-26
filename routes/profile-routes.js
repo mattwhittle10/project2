566e89f21c5bc2b155b3b3834e72cd1e90ad5759
Nathan Elorreaga <nathan.elorreaga@gmail.com>
2019-08-25 23:22:06 -0600

    got cards to render on Home page and User Home Page

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,24 +1,31 @@
 const router = require('express').Router();
 const authroutes = require('./auth-routes');
+const listing = require('../models/storage')
 
-const authCheck = (req, res, next) =>{
-    if(!req.user){
+const authCheck = (req, res, next) => {
+    if (!req.user) {
         //if user is not logged in
         res.redirect('/auth/login');
-    }else{
+    } else {
         //if they are logged in
         next();
     }
 };
 
-router.get('/', authCheck, (req,res) =>{
+router.get('/', authCheck, (req, res) => {
     //profile page will render here! passing in the user object with all of the details 
     //send in a view here
-    res.render('profile', {user:req.user});
+    listing.allListings(function (data) {
+        var listings = data;
+        console.log(listings)
+        res.render('profile', { user: req.user, listings });
+    })
 });
 
-router.get('/update', (req, res) =>{
-    res.render('update-profile', {user:req.user});
+router.get('/update', (req, res) => {
+    res.render('update-profile', { user: req.user });
 });
+
+
 
 module.exports = router;
