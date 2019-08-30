cfff3c434bc3d2f81895cc31d385b0a9f4ab6f8d
Calli Crase <callilogancrase@gmail.com>
2019-08-30 11:23:51 -0600

    adding calendar dependencies

---
+++ public/assets/js/each-listing.js
@@ -0,0 +1,6 @@
+dycalendar.draw({
+    //targeting the element with id=calendar --> div containing the actual calendar.
+    target: "#calendar"
+});
+
+console.log("made to the each-listing.js file");
