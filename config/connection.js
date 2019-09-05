f50e6353838a6e17d8ff162d4b8bd6236b701bee
Whittle <matt.whittle10@gmail.com>
2019-09-05 17:46:05 -0600

    changed connection.js to have JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql');
+var connection
 
-var connection = mysql.createConnection({
-    port: process.env.MYSQL_PORT,
-    user: "root",
-    password: "root",
-    database: "storage_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: process.env.MYSQL_PORT,
+        user: "root",
+        password: "root",
+        database: "storage_db"
+    });
+};
 
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("DB connection successful");
 });
 
